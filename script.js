2f8c0240498681c873be2069b9191e0716f874db
S1123576 <repellent0830@gmail.com>
2025-01-08 16:36:53 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// 滾動特效
+ScrollReveal().reveal('.hero-content', { delay: 200, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('.feature-item', { interval: 200, delay: 300, distance: '30px', origin: 'bottom' });
+ScrollReveal().reveal('blockquote', { interval: 300, distance: '20px', origin: 'left' });
